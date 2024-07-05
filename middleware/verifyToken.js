e812cbff430730a4ab18423a94d3cf8eb335e06d
Tri Le <trlminh120202@gmail.com>
2024-07-06 04:26:48 +0700

    nnnn

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -2,10 +2,10 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 const verifyToken = (req, res, next) => {
-    const authHeader = req.headers.authorization;
+    const authHeader = req.headers.token;
     if (authHeader) {
         const token = authHeader.split(" ")[1];
-        jwt.verify(token, process.env.JWT_SEC, async (err, user) => {
+        jwt.verify(token, process.env.JWT_SEC, (err, user) => {
             if (err) return res.status(403).json("Invalid token");
             req.user = user;
             next();
